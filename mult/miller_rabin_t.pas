a437cfc9ff0280632c73c8621ec6574c6fd0aab4
JulStrat <ioulianos.kakoulidis@hotmail.com>
2020-10-11 15:15:56 +0300

    Miller-Rabin test

--- mult/miller_rabin_t.pas
+++ mult/miller_rabin_t.pas
@@ -38,6 +38,22 @@ begin
       if MillerRabin(n) then Halt(1);
     Inc(n);
   end;
-  
+
+  n := 1000000000000000000;
+  primesieve_skipto(it, 1000000000000000000, 1000000000001000000);
+  prime := primesieve_next_prime(it);
+
+  while n <= 1000000000001000000 do
+  begin
+    if n = prime then
+    begin
+      if not MillerRabin(n) then Halt(1);
+      prime := primesieve_next_prime(it);
+    end
+    else
+      if MillerRabin(n) then Halt(1);
+    Inc(n);
+  end;
+
   primesieve_free_iterator(it);
 end.
